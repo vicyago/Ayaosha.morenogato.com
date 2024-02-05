d64ff01a99f072b620842815aa04c4a25ea95b51
vicyago <victoryagoc@gmail.com>
2024-02-05 00:28:08 -0300

    news

--- js/popup.js
+++ js/popup.js
@@ -4,19 +4,23 @@ var modal = document.getElementById("myModal");
 // Get the modal content
 var modalContent = document.getElementsByClassName("modal-content")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var btn = document.getElementById("myBtnMobile");
+// Get the buttons that open the modal
+var btnDesktop = document.getElementById("myBtn");
+var btnMobile = document.getElementById("myBtnMobile");
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
-btn.onclick = function () {
+// Function to open the modal
+var openModal = function () {
     modal.style.display = "block";
-
     // Reset the top position of the modal content
     modalContent.style.top = "0";
 };
+
+// When the user clicks the buttons, open the modal
+btnDesktop.onclick = openModal;
+btnMobile.onclick = openModal;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
